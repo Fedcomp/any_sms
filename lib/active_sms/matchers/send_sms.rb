f79c1890b9fbc93dc95cd381c0ed215ba8c9b306
Fedcomp <aglergen@gmail.com>
2016-11-10 16:00:42 +0300

    [rspec] send_sms matcher, first version

---
+++ lib/active_sms/matchers/send_sms.rb
@@ -0,0 +1,68 @@
+require "rspec/matchers"
+
+# rubocop:disable Metrics/BlockLength
+RSpec::Matchers.define :send_sms do |options|
+  supports_block_expectations
+
+  match do |block|
+    options ||= {}
+    @phone = options.delete(:to)
+    @text  = options.delete(:text)
+
+    # we need to know actual phone and text for failure message
+    called = false
+    allow(ActiveSMS).to receive(:send_sms) do |actual_phone, actual_text|
+      puts "called"
+      @actual_phone = actual_phone
+      @actual_text = actual_text
+      called = true
+
+      result
+    end
+
+    block.call
+
+    expect(called).to be(true)
+    if @phone && @text
+      expect(@actual_phone).to eq(@phone)
+      expect(@actual_text).to eq(@text)
+    end
+
+    true
+  end
+
+  chain :and_return do |return_options|
+    @result = ActiveSMS::Response.new(return_options)
+  end
+
+  failure_message do
+    message = "expected block to send "
+
+    if @phone && @text
+      message += "sms message" \
+                 " to phone number \"#{@phone}\"" \
+                 " with text \"#{@text}\""
+    else
+      message += "any sms message"
+    end
+
+    "#{message}, nothing was sent"
+  end
+
+  failure_message_when_negated do
+    "expected block to not send any sms message, " \
+    "instead sent sms to phone number \"#{@actual_phone}\" " \
+    "with message \"#{@actual_text}\""
+  end
+
+  # Not tested
+  description do
+    "send sms"
+  end
+
+  private
+
+  def result
+    @result ||= ActiveSMS::Response.new(status: :success)
+  end
+end
---
+++ spec/active_sms/matchers/send_sms_spec.rb
@@ -0,0 +1,93 @@
+require "spec_helper"
+require "active_sms/matchers/send_sms"
+
+describe "SendSMS matcher" do
+  it "pass if sms was sent" do
+    expect { ActiveSMS.send_sms("", "") }.to send_sms
+  end
+
+  it "does not pass if sms was not sent" do
+    expect {}.not_to send_sms
+  end
+
+  it "accept only blocks"
+
+  it "returns mocked response object" do
+    result = nil
+    expect { result = ActiveSMS.send_sms("", "") }.to send_sms
+    expect(result.class).to be(ActiveSMS::Response)
+  end
+
+  context "with recepient and text specified" do
+    it "pass if they match" do
+      expect do
+        ActiveSMS.send_sms("+10000000000", "test text")
+      end.to send_sms(to: "+10000000000", text: "test text")
+    end
+
+    it "does not pass in case they don't match" do
+      expect do
+        ActiveSMS.send_sms("+20000000000", "test text")
+      end.not_to send_sms(to: "+10000000000", text: "test text")
+
+      expect do
+        ActiveSMS.send_sms("+10000000000", "test message")
+      end.not_to send_sms(to: "+10000000000", text: "test text")
+    end
+  end
+
+  context "with return options" do
+    before do
+      @result = nil
+      expect { @result = ActiveSMS.send_sms("", "") }.to send_sms
+        .and_return(status: :success, meta: { funds: 40 })
+    end
+
+    it "pass status to response object" do
+      expect(@result.status).to eq(:success)
+    end
+
+    it "pass meta to response object" do
+      expect(@result.meta).to eq(funds: 40)
+    end
+  end
+
+  context "error messages" do
+    it "for default usage when expectation didn't met" do
+      expect do
+        expect {}.to send_sms
+      end.to raise_error(
+        RSpec::Expectations::ExpectationNotMetError,
+        "expected block to send any sms message, nothing was sent"
+      )
+    end
+
+    it "when phone number and text specified" do
+      expect do
+        expect {}.to send_sms(to: "+10000000000", text: "test")
+      end.to raise_error(
+        RSpec::Expectations::ExpectationNotMetError,
+        'expected block to send sms message ' \
+        'to phone number "+10000000000" ' \
+        'with text "test", nothing was sent'
+      )
+    end
+
+    context "when negated" do
+      it "for default usage when expectation suddenly met" do
+        expect do
+          expect { ActiveSMS.send_sms("+10000000000", "test") }.to_not send_sms
+        end.to raise_error(
+          RSpec::Expectations::ExpectationNotMetError,
+          'expected block to not send any sms message, instead ' \
+          'sent sms to phone number "+10000000000" ' \
+          'with message "test"'
+        )
+      end
+    end
+  end
+
+  context "description message" do
+    it "is expected to include phone and text"
+  end
+end
